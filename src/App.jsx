678c97f1f910f9d610ce23cd65f0d7de4fdd8f95
Josh Yam <jyam941@gmail.com>
2025-03-11 10:39:16 -0400

    chatbot app

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  // useState react hook. "useState("")" is the intial variable, in this case an empty string. setMessage() is the function I will use to set it to the next state.
+  const [message, setMessage] = useState("");
+  const [response, setResponse] = useState("");
+
+  function sendMessage(e) {
+    setMessage(e.target.value);
+  }
+
+
+
+
+  return (
+    <>
+    <div className="input-container">
+      <h1>How can I help today?</h1>
+      <input type="text" value={message} onChange={sendMessage} className="search-bar" placeholder="Ask Anything"></input>
+      <button className="submit-btn" onClick={sendMessage}>Submit</button>
+      <h2>Response: {message}</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
